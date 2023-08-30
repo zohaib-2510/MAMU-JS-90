470569650327a62c0dcef543fc7b1d0a1ae94c57
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-30 04:25:31 -0700

    FULL FULL COMPLETE
    FULL FULL COMPLETE

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
 var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
 var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
 var totsl =  mark1 + mark2 + mark3
+var total = +prompt('3 SUBJECT KE TOTAL MARK DALO DEKO AGR 1 SUB KE 100 HAIN TO 3 KE 300 DALOOO')
 var perc = (totsl / 300) * 100 
- document.write('YOUR TOTAL MARKS OF ALL SUBJECT IS'+' '+ totsl+' '+'AND YOUR PERCENTAGE IS'
+ document.write('YOUR TOTAL MARKS OF ALL SUBJECT IS'+' '+ totsl+' '+'OUT OF'+' '+total+' '+'AND YOUR PERCENTAGE IS'
  +' '+perc.toFixed(2)+"%"+ ' '+ 'OUT OF 100%')
