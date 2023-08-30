410c2025abcb5cf9dcecebe5d570a59a189b5aeb
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-30 04:06:55 -0700

    COMPLETE

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
 var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
 var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
 var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
-
-
 var totsl =  mark1 + mark2 + mark3
 var perc = (totsl / 300) * 100 
-
  document.write('YOUR TOTAL MARKS OF ALL SUBJECT IS'+' '+ totsl+' '+'AND YOUR PERCENTAGE IS'
  +' '+perc+"%"+ ' '+ 'OUT OF 100')
